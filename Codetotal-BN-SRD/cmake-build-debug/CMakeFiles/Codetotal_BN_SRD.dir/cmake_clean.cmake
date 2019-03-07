277a2e4100fdf86ab1b3f5f5da3b0e67bfcdcddd
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2019-03-07 15:44:38 +0100

    Création projet C depuis CLion

---
+++ Codetotal-BN-SRD/cmake-build-debug/CMakeFiles/Codetotal_BN_SRD.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Codetotal_BN_SRD.dir/main.c.obj"
+  "Codetotal_BN_SRD.pdb"
+  "Codetotal_BN_SRD.exe"
+  "Codetotal_BN_SRD.exe.manifest"
+  "libCodetotal_BN_SRD.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Codetotal_BN_SRD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Codetotal-BN-SRD/cmake-build-debug/CMakeFiles/Codetotal_BN_SRD.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Codetotal_BN_SRD.
+# This may be replaced when dependencies are built.
