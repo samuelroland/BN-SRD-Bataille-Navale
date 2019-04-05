86f535cfe99a85bd1a26271cd66e0399d6389ad4
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2019-04-05 09:19:33 +0200

    Correction problème de clearscreen si série de lettres random, pour tirer une case

--- Codetotal-BN-SRD/main.c
+++ Codetotal-BN-SRD/main.c
@@ -197,7 +197,7 @@ void Affgrille2() {  //autre manière de faire la fonction.
 }
 
 int result = -2; //-2=pas de résultat, -1=déja tiré ici, 0=a l'eau, 1=touché, 2=touché coulé.
-char hits[2];   //deux cases pour les coups.
+char hits[100];   //deux cases pour les coups.
 int batotouches[4] = {0, 0, 0, 0};    //Cases touchées par bateaux:    0=inutile! donc 4 cases.
 int compt_batotouches = 0;  //compteur de bateaux touchés. dès que =3 alors victoire.
 
